5e7343c5ca36f282b10c00900eec7129759cfa1c
Luis Alfonso <130397061+ThePocho78@users.noreply.github.com>
2024-02-09 11:45:03 -0600

    Delete subpagina directory

--- subpagina/js.js
+++
@@ -1,7 +0,0 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-
-function nextSlide() {
-  carouselSlide.style.transform = 'translateX(-100%)';
-}
-
-setInterval(nextSlide, 3000); // Cambia de diapositiva cada 3 segundos
